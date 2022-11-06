17b5a810f47a7feca9e7763c4d583c1d04071952
Wafa Berri <wberri13@gmail.com>
2022-11-06 17:50:30 -0500

    dfsdfsfsdfs

--- src/Main.java
+++ src/Main.java
@@ -29,6 +29,17 @@ public class Main {
         System.out.println(fun.maxDoubles(8, 5));
         System.out.println(fun.maxDoubles(10, 456000));
 
+        System.out.println("-- test isPrime --");
+        System.out.println(fun.isPrime(2));
+        System.out.println(fun.isPrime(13));
+        System.out.println(fun.isPrime(773));
+        System.out.println(fun.isPrime(9857));
+        System.out.println(fun.isPrime(101879));
+        System.out.println(fun.isPrime(1));
+        System.out.println(fun.isPrime(14));
+        System.out.println(fun.isPrime(30));
+        System.out.println(fun.isPrime(771));
+        System.out.println(fun.isPrime(93243));
 
     }
 }
